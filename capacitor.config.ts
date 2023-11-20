3b04e456a0d51150197338fdba9c226e58ebad8d
RUGVED RAIKWAR <rugvedraikwar@RUGVEDs-MacBook-Air.local>
2023-11-21 04:44:15 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'VisionKalyanApp',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
